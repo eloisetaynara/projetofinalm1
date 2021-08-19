9ab43cfc331ecf2dcb6338303152dfa518f1be8a
eloisecoffe <61201063+eloisecoffe@users.noreply.github.com>
2021-08-19 15:22:21 -0300

    finalização do projeto

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,154 @@
-console.log("Fui carregado de um arquivo externo")
-var titulo = document.querySelector(".titulo")
-titulo.textContent = "Aparecida Nutricionista"
+// PARTE DO DRACONATO
+
+function escolha1drag(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/fases/dracon/fase2.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+function escolha2drag(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/fases/dracon/fase3.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+function escolha3drag(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/final/yes.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+// PARTE DA ELFA
+
+function escolha1elf(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/fases/elfa/fase2.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+function escolha2elf(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/fases/elfa/fase3.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+function escolha3elf(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/final/yes.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+// PARTE TRANSMORFO
+
+function escolha1trans(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/fases/transmorfo/fase2.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+function escolha2trans(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/fases/transmorfo/fase3.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+function escolha3trans(){
+    var x;
+
+    var opcao=prompt("Digite sua opção:");
+
+	if(opcao == 1){
+		window.location="/final/yes.html";
+	}
+    else if (opcao == 2) {
+        window.location="/final/over.html"
+    }
+    else {
+        return alert("ERRO. APENAS NÚMERO.")
+    }
+}
+
+function voltar() {
+    alert("Você será redirecionado");
+	window.location="/index.html";
+}
